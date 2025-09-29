893643795a98dd7632115fc3f573f4d25bffa3a6
Simon Loir <simon-loir@hotmail.com>
2025-09-29 20:26:53 +0200

    feat: extend invoice schema with invoicePeriod and additional fields

--- src/documents/invoices/Invoice.ts
+++ src/documents/invoices/Invoice.ts
@@ -4,6 +4,12 @@ import { z } from 'zod';
 
 const date = z.union([z.string(), z.date()]);
 
+
+const invoicePeriod = z.object({
+    startDate: date.optional(),
+    endDate: date.optional(),
+    descriptionCode: z.enum(['3', '35', '432'])
+});
 export const invoiceSchema = z.object({
     ID: z.string().min(1),
     issueDate: date,
@@ -12,6 +18,10 @@ export const invoiceSchema = z.object({
     note: z.string().optional(),
     taxPointDate: date.optional(),
     documentCurrencyCode: CurrencyCodeSchema,
+    taxCurrencyCode: CurrencyCodeSchema.optional,
+    accountingCost: z.string().optional(),
+    buyerReference: z.string().optional(),
+    invoicePeriod: invoicePeriod.optional()
 });
 
 export type Invoice = z.infer<typeof invoiceSchema>;
