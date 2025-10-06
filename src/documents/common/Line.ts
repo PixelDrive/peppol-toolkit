502b5919924360e9f27f73613c72686dc79750b0
SimonLoir <simon@pixeldrive.be>
2025-10-06 16:09:31 +0200

    feat: enhance `lineSchema` with additional fields and modular schemas
    - Added new fields: `name`, `description`, `originCountry`, `taxCategory`, `price`, and `currency`.
    - Integrated reusable schemas for `CountryCode`, `TaxCategory`, and `CurrencyCode`.
    - Expanded support for detailed line item representation.

--- src/documents/common/Line.ts
+++ src/documents/common/Line.ts
@@ -1,5 +1,8 @@
 import { z } from 'zod';
 import { date } from './Date';
+import { CountryCodeSchema } from './CountryCodes';
+import { taxCategorySchema } from './TaxCategory';
+import { CurrencyCodeSchema } from './CurrencyCodes';
 
 export const lineSchema = z.object({
     id: z.string().min(1),
@@ -15,4 +18,21 @@ export const lineSchema = z.object({
         .optional(),
     orderLineReference: z.string().optional(),
     documentReference: z.string().optional(),
+    //TODO: add AllowanceCharge
+    name: z.string(),
+    description: z.string().optional(),
+    buyersItemIdentification: z.string().optional(),
+    sellersItemIdentification: z.string().optional(),
+    standardItemIdentification: z.string().optional(),
+    originCountry: CountryCodeSchema.optional(),
+    //TODO: add item classification
+    taxCategory: taxCategorySchema.pick({
+        categoryCode: true,
+        percent: true,
+    }),
+    additionalItemProperties: z.record(z.string(), z.any()).optional(),
+    price: z.number().min(0),
+    currency: CurrencyCodeSchema,
+    //TODO: add price allowance and baseQT
+    unitCode: z.string().optional(),
 });
